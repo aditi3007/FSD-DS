afdd8f1cde8dcb85dfbb8eaf7e6660fc668f5b59
Aditi Bhargava <121795623+aditi3007@users.noreply.github.com>
2024-10-17 22:56:35 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function validate() {
-  let un = document.getElementById("un").value;
-  let pass = document.getElementById("pass").value;
-  if (un == "admin" && pass == "admin")
-    document.getElementById("res").innerHTML = "Login Success";
-  else document.getElementById("res").innerHTML = "Login Failed";
-}
