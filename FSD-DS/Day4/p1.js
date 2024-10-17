02e8e4759a67d1f2bba176a81a7bc9b16c62e244
aditi3007 <aditirdc@gmail.com>
2024-10-17 22:33:15 +0530

    ArrowFunction

---
+++ FSD-DS/Day4/p1.js
@@ -0,0 +1,27 @@
+// let x = '12';
+// let num = Number(x);
+// let y ='12'%'4';
+// let b = Boolean(12);
+// console.log(b);
+// function display(){
+//     // let c = confirm("Are you sure to Logout");
+//     let c = prompt("Enter your Name","Rahul");
+//     alert(c);
+// }
+
+
+
+// let a = 12;
+// let b = '12';
+// console.log(a==b);
+// console.log(a===b);
+
+// let sayhello = ()=> console.log("Arrow Function");
+// sayhello();
+
+let sum = (x,y) =>{
+    return x + y;
+};
+let res = sum(12,34);
+console.log("Result is " +res);
+console.log(sum(12,34));
